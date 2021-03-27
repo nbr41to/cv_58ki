1a2bab14f767edc0b3685948985ed70c207a28a7
nbr41to <nbr.41to@gmail.com>
2021-03-27 11:07:03 +0900

    .

--- dice.js
+++ dice.js
@@ -1,4 +1,16 @@
-const students = ['吉村', '天辰', '成田', '加藤', '上内',];
-const dice = Math.floor(Math.random() * students.length);
-console.log(students[dice]);
-document.write(students[dice]);
+const students = [
+  '吉村',
+  '天辰',
+  '成田',
+  '加藤',
+  '上内',
+];
+let dice = Math.floor(Math.random() * students.length);
+
+const taget = document.getElementById('target');
+taget.textContent = `${students[dice]}さん`;
+
+const redice = () => {
+  dice = Math.floor(Math.random() * students.length);
+  taget.textContent = `${students[dice]}さん`;
+};
