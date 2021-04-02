815e8e46c604251b3ec7e649cb1574537bf5c3b4
Itsuki1218 <78390796+Itsuki1218@users.noreply.github.com>
2021-04-03 08:04:34 +0900

    課題5の提出

--- lesson.js
+++ lesson.js
@@ -27,3 +27,35 @@ console.log(array) //=> [2]
 array[0] = 100
 console.log(array) //=> [100]
 
+///////////////////////////
+//   ----- 課題5-------　//
+///////////////////////////
+
+//課題05-1//
+
+const items = [
+    { n: 100, },
+    { a: { b: ["abc"] }, c: { ['100']: 'footer' } },
+]
+
+console.log(items[0].n) //=> 100
+console.log(items[1].a.b[0]) //=> abc
+console.log(items[1].c['100']) //=> foobar
+
+//課題05-2//
+
+const json = `
+    {
+   "total": "2",
+   "items":  [{"name":"Item123","id": "123"},{"name":"Item456","id":"456"}],
+   "next":{"id":789}
+}
+`
+
+const obj = JSON.parse(json)
+console.log(obj.total) // => 2
+console.log(obj.items[0].name) //=> Item 123
+console.log(obj.items[0].id) //=> 123
+console.log(obj.items[1].name) //=> Item 456
+console.log(obj.items[1].id) //=> 456
+console.log(obj.next.id) //=> 789
