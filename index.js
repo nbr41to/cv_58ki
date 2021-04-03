2d1780d2fe86efa0718784f142c1b4f4f092b49a
nbr41to <nbr.41to@gmail.com>
2021-04-03 13:03:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,105 @@
-// for文 処理を繰り返す
+// // for文 処理を繰り返す
 
-const nums = [1, 2, 3, 4, 5];
+// const nums = [1, 2, 3, 4, 5];
 
-for (let i = 0; i < 5; i++) {
-  // 繰り返す処理
-  console.log(nums[i] * 2);
-}
-
-// while文 処理を繰り返す
-
-// let i = 0;
-
-// while (i < 5) {
+// for (let i = 0; i < 5; i++) {
 //   // 繰り返す処理
 //   console.log(nums[i] * 2);
-//   i++;
 // }
 
-// do while
+// // while文 処理を繰り返す
 
-let power = 99;
-let i = 0;
+// // let i = 0;
 
-do {
-  if (power >= 99) {
-    console.log('powerは最大です');
-  } else {
-    power += 1;
-    i++;
-  }
-  power += 1;
-  i++;
-} while (i < 3);
+// // while (i < 5) {
+// //   // 繰り返す処理
+// //   console.log(nums[i] * 2);
+// //   i++;
+// // }
 
-console.log(power);
+// // do while
+
+// let power = 98;
+// let i = 0;
+
+// do {
+//   console.log(`今のpower:${power}`);
+//   if (power >= 99) {
+//     console.log('powerは最大です');
+//     i += 3;
+//   } else {
+//     power += 1;
+//     i++;
+//   }
+// } while (i < 3);
+
+// console.log(power);
+
+// // 関数
+
+// function iii() {
+
+// }
+
+// const aaa = function () {
+
+// };
+
+// // アロー関数
+// const uuu = () => {
+//   console.log(power);
+//   console.log(power);
+//   console.log(power);
+//   console.log(power);
+//   console.log(power);
+//   console.log(power);
+// };
+
+// // 戻り値
+// const stringReturn = () => {
+//   if ('aa') {
+
+//     return ("文字だよ");
+//   }
+// };
+
+// console.log(stringReturn());
+
+// // 引数
+
+// const greet = (name, age) => {
+//   console.log(`こんにちは！${name}さん.あなたは${age}歳です`);
+// };
+
+// greet(123, 'nobuyuki');
+
+// メソッド
+// 関数？を呼び出す
+
+const aaa = 'adadadadada';
+const bbb = [1, 2, 3, 4, 5, 6,];
+
+const cards = [
+  { suit: '♥', num: 1 },
+  { suit: '◆', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+  { suit: '♥', num: 1 },
+];
+
+if (cards[0].num === cards[0].num) {
+  point++;
+}
