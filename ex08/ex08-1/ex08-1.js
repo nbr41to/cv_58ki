13a014de90bfd5bc5698dc018a8cf4c17803f038
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-04-04 23:49:53 +0900

    ex08

---
+++ ex08/ex08-1/ex08-1.js
@@ -0,0 +1,14 @@
+// -- ★　--
+const link = document.querySelector("a");
+console.log(link.textContent);
+
+
+// -- ★★　--
+const todo = document.getElementById("todo");
+console.log(todo.children[0].textContent);
+
+
+// -- ★★★　--
+const
+list = todo.querySelector('.list');
+console.log(list.children[1].textContent);
