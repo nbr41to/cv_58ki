cbb0d80e10e75f97b6b0ad30633326d9a6795bbe
Itsuki1218 <78390796+Itsuki1218@users.noreply.github.com>
2021-04-09 08:07:22 +0900

    ファイルを整理しました

--- ex05.js
+++ ex05.js
@@ -1,33 +1,4 @@
 ///////////////////////////
-//   ----- 課題３-------　//
-///////////////////////////
-
-//課題03-2//
-
-console.log("I'm learning JavaScript." + "\n" + '\nJavaScript says "Hello, world!"'
-)
-
-//課題03ー3//
-
-const array = [1, 2, 3]
-
-// ここに回答のプログラムを書く
-array.splice(0, 1)
-console.log(array) //=> [2, 3]
-
-// ここに回答のプログラムを書く
-array.push(5)
-console.log(array) //=> [2, 3, 5]
-
-// ここに回答のプログラムを書く
-array.splice(1)
-console.log(array) //=> [2]
-
-// ここに回答のプログラムを書く
-array[0] = 100
-console.log(array) //=> [100]
-
-///////////////////////////
 //   ----- 課題5-------　//
 ///////////////////////////
 
