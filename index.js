83a0b240aed5edb434559c497a6fe285e4c2b421
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-04-29 16:29:58 +0900

    update

--- index.js
+++ index.js
@@ -2,81 +2,92 @@ document.getElementById("hand").style.visibility = "hidden";
 
 function start() {
   const hand = document.getElementById("hand");
-	if(hand.style.visibility=="visible"){
-		// hiddenで非表示
-		hand.style.visibility ="hidden";
-    document.querySelector('.btn_start').innerHTML = "Start";
-    document.getElementById('result').innerHTML = "";
-	}else{
-		// visibleで表示
-		hand.style.visibility ="visible";
+  if (hand.style.visibility == "visible") {
+    // hiddenで非表示
+    hand.style.visibility = "hidden";
+    document.querySelector(".btn_start").innerHTML = "Start";
+    document.getElementById("result").innerHTML = "";
+  } else {
+    // visibleで表示
+    hand.style.visibility = "visible";
   }
 }
 
 function janken(playerSelect) {
   const cpuSelect = Math.floor(Math.random() * 3);
   const hand = ["✊", "✌", "✋"];
-  const gu = document.querySelector('.btn_gu')
-  const cho = document.querySelector('.btn_cho')
-  const pa = document.querySelector('.btn_pa')
+  const gu = document.querySelector(".btn_gu");
+  const cho = document.querySelector(".btn_cho");
+  const pa = document.querySelector(".btn_pa");
 
   if (playerSelect === 0) {
-    gu.setAttribute("disabled", true)
+    gu.setAttribute("disabled", true);
+    cho.setAttribute("disabled", true);
+    pa.setAttribute("disabled", true);
   }
   if (playerSelect === 1) {
-    cho.setAttribute("disabled", true)
+    gu.setAttribute("disabled", true);
+    cho.setAttribute("disabled", true);
+    pa.setAttribute("disabled", true);
   }
   if (playerSelect === 2) {
-    pa.setAttribute("disabled", true)
+    gu.setAttribute("disabled", true);
+    cho.setAttribute("disabled", true);
+    pa.setAttribute("disabled", true);
   }
 
-  if (playerSelect === cpuSelect) { // あいこ
+  if (playerSelect === cpuSelect) {
+    // あいこ
     result = "あいこです。";
   } else {
     // あいこじゃなかったら勝敗を決める
-    if (playerSelect === 0) { // playerはグー
-      if (cpuSelect === 1) { // cpuはチョキ
+    if (playerSelect === 0) {
+      // playerはグー
+      if (cpuSelect === 1) {
+        // cpuはチョキ
         result = "勝ちました。";
-      } else { // cpuはパー
+      } else {
+        // cpuはパー
         result = "負けました。";
       }
-
-    } else if (playerSelect === 1) { // playerはチョキ
-      if (cpuSelect === 0) { // cpuはグー
+    } else if (playerSelect === 1) {
+      // playerはチョキ
+      if (cpuSelect === 0) {
+        // cpuはグー
         result = "負けました。";
-      } else { // cpuはパー
+      } else {
+        // cpuはパー
         result = "勝ちました。";
       }
-
-    } else { // playerはパー
-      if (cpuSelect === 0) { // cpuはグー
+    } else {
+      // playerはパー
+      if (cpuSelect === 0) {
+        // cpuはグー
         result = "勝ちました。";
-      } else { // cpuはチョキ
+      } else {
+        // cpuはチョキ
         result = "負けました。";
       }
     }
   }
 
-
   if (result === "負けました。") {
-    document.getElementById('result').innerHTML = `
+    document.getElementById("result").innerHTML = `
     <p>You Lose…</P>
     <P>あなた：${hand[playerSelect]}</P>
     <P>CPU：${hand[cpuSelect]}</P>`;
-
   } else if (result === "勝ちました。") {
-    document.getElementById('result').innerHTML = `
+    document.getElementById("result").innerHTML = `
     <p>You Win!!</P>
     <P>あなた：${hand[playerSelect]}</P>
     <P>CPU：${hand[cpuSelect]}</P>`;
-
   } else {
-    document.getElementById('result').innerHTML = `
+    document.getElementById("result").innerHTML = `
     <p>Drow</P>
     <P>あなた：${hand[playerSelect]}</P>
     <P>CPU：${hand[cpuSelect]}</P>`;
   }
 
   console.log(result);
-  document.querySelector('.btn_start').innerHTML = "もう一度やる";
+  document.querySelector(".btn_start").innerHTML = "もう一度やる";
 }
